aa34ed7da80ffdcc43421476579ca3052b6496f3
Dylan Storey <dstorey@gmail.com>
2024-12-31 20:41:08 -0500

    updating CI/CD commands

--- .angreal/task_tests.py
+++ .angreal/task_tests.py
@@ -65,8 +65,8 @@ def unit_tests(crate_name: str, test_filter: str = ""):
 @test()
 @angreal.command(name="integration", about="run integration tests for a specific crate")
 @angreal.argument(name="test_filter", required=False, help="Filter for specific tests or modules")
-@angreal.argument(name="crate_name", required=True, help= f"Name of the crate to test ({CRATES + ['all']})")
 @angreal.argument(name="skip_docker", required=False, help="Skip docker compose up", takes_value=False, is_flag=True)
+@angreal.argument(name="crate_name", required=True, help= f"Name of the crate to test ({CRATES + ['all']})")
 def integration_tests(crate_name: str, test_filter: str = "", skip_docker: bool = False):
     """Run integration tests for a specific crate."""
     if not skip_docker:
